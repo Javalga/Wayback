2492e72e37ce2247d9aa81954032e9623a121e27
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-13 11:28:15 +0100

    routing configured

--- angular-wayback/src/app/app-routing.module.ts
+++ angular-wayback/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { CustomerInputComponent } from './pages/customer-input/customer-input.component'
+import { HistoricalComponent } from './pages/historical/historical.component';
+import { IncidentInputComponent } from './pages/incident-input/incident-input.component';
+import { IncidentOutputComponent } from './pages/incident-output/incident-output.component';
+import { ReturnsComponent } from './pages/returns/returns.component';
+import { SolvedPullComponent } from './pages/solved-pull/solved-pull.component';
+import { UserAdminComponent } from './pages/user-admin/user-admin.component';
 
 
-const routes: Routes = [{ path: "login", component: LoginComponent },
-{ path: "", redirectTo: "/login", pathMatch: "full" }
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'costumer-input', component: CustomerInputComponent },
+  { path: 'historical', component: HistoricalComponent },
+  { path: 'incident-input', component: IncidentInputComponent },
+  { path: 'incident-output', component: IncidentOutputComponent },
+  { path: 'returns', component: ReturnsComponent },
+  { path: 'solved-pull', component: SolvedPullComponent },
+  { path: 'user-admin', component: UserAdminComponent },
 ];
 
 @NgModule({
