1a2956c8ddbb16cd00580bd26c02c23847f207f5
AlejoPrzHub <josealeprz@gmail.com>
2022-12-14 17:58:50 +0100

    "solved-pull"

--- angular-wayback/src/app/pages/solved-pull/solved-pull.component.ts
+++ angular-wayback/src/app/pages/solved-pull/solved-pull.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./solved-pull.component.css']
 })
 export class SolvedPullComponent {
-  public value: string = "Imprimir Etiquetas de Incidencias Solucionadas"
+  public value: string = "Imprimir Etiquetas de Incidencias Solucionadas";
+  public cols: string[];
+  public rows: any
+  public show = true;
+
+  constructor()
+  {
+    this.cols =["Indice","Seleccionar","N* Expedicion","F.Entrada","F.Salida","X","X","Etiqueta","Formulario"];
+    this.rows = [
+      ["v", "123456789", "12/12/1212", "15/15/1515", "X", "X", "Etiqeuta", "formulario"],
+      ["v", "123456789", "12/12/1212", "15/15/1515", "X", "X", "Etiqeuta", "formulario"],
+      ["v", "123456789", "12/12/1212", "15/15/1515", "X", "X", "Etiqeuta", "formulario"],
+      ["v", "123456789", "12/12/1212", "15/15/1515", "X", "X", "Etiqeuta", "formulario"],
+      ["v", "123456789", "12/12/1212", "15/15/1515", "X", "X", "Etiqeuta", "formulario"]
+    ]
+  }
+
+  swap()
+  {
+    this.show = this.show?false:true
+  }
+
+
 }
