85c156ff66610136d32d0b3009d887ab3fd0540f
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-15 17:18:59 +0100

    m

--- angular-wayback/src/app/shared/login.service.ts
+++ angular-wayback/src/app/shared/login.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
-  public isLogged: boolean = true;
+  public isLogged: boolean = false;
   constructor() {
   }
 }
