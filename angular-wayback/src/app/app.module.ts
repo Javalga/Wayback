d6c3ac20c6bc96f9ff4067384d2b824255dc5389
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-22 10:54:29 +0100

    forms problems

--- angular-wayback/src/app/app.module.ts
+++ angular-wayback/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { IncidentInputComponent } from './pages/incident-input/incident-input.component';
