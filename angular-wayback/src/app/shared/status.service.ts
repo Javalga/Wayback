95a3eeccc81838793b4338cb73cfd0182be47590
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-12-23 20:29:58 +0100

    m

--- angular-wayback/src/app/shared/status.service.ts
+++ angular-wayback/src/app/shared/status.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Status } from 'src/app/models/status';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StatusService {
-
   public status: Status[];
-  constructor() { 
-    this.status = [new Status('En Almacén'), new Status('Entrega Concertada'),new Status("Devuelto"),new Status("Rechazo Confirmado"),new Status("Puesto en Reparto")];
+  public url: string;
+  constructor(private http: HttpClient) {
+    // this.status = [new Status('En Almacén'), new Status('Entrega Concertada'),new Status("Devuelto"),new Status("Rechazo Confirmado"),new Status("Puesto en Reparto")];
+  }
+  public getStatus() {
+    this.url = 'http://localhost:3000/status';
+    return this.http.get(this.url);
+  }
+
+  public putStatus(params) {
+    this.url = 'http://localhost:3000/status';
+    return this.http.put(this.url, params);
   }
 }
