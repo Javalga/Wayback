7b7dcada7e9f0d94647209672b99e51db16d4a17
Javier Fernández Jiménez <javifav92@gmail.com>
2022-12-27 20:53:52 +0100

    dates-range

--- angular-wayback/src/app/components/dates-range/dates-range.component.ts
+++ angular-wayback/src/app/components/dates-range/dates-range.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { AsideHeaderService } from 'src/app/shared/aside-header.service';
 
 @Component({
@@ -6,7 +7,7 @@ import { AsideHeaderService } from 'src/app/shared/aside-header.service';
   templateUrl: './dates-range.component.html',
   styleUrls: ['./dates-range.component.css']
 })
-export class DatesRangeComponent {
+export class DatesRangeComponent implements OnInit {
 
   @ViewChild('dateSince') dateSince;
   @ViewChild('dateUntil') dateUntil;
@@ -16,8 +17,49 @@ export class DatesRangeComponent {
 
 
   }
+  ngOnInit(): void {
+
+  }
+
+  public defaultDateSince() {
+
+    if (this.asideHeaderService.state === 'Dashboard' || this.asideHeaderService.state === 'Pool de solucionadas') {
+      return this.twoWeeksAgo();
+    } else if (this.asideHeaderService.state === 'Devoluciones' || this.asideHeaderService.state === 'Histórico') {
+      return "1970-01-01"
+    }
+
+  }
+
+
+  public defaultDateUntil() {
+
+    if (this.asideHeaderService.state === 'Dashboard' || this.asideHeaderService.state === 'Histórico') {
+      return this.today();
+    } else if (this.asideHeaderService.state === 'Devoluciones') {
+      return this.twoWeeksAgo();
+    } else return this.tomorrow();
+
+  }
+
+
+  public twoWeeksAgo() {
+
+    return moment().subtract(15, "days").format("YYYY-MM-DD");
+  }
+
+  public tomorrow() {
+
+    return moment().add(1, "days").format("YYYY-MM-DD");
+  }
+
+  public today() {
+
+    return moment().format("YYYY-MM-DD")
+  }
 
   public sendDates() {
+
     this.asideHeaderService.dateSince = this.dateSince.nativeElement.value;
     this.asideHeaderService.dateUntil = this.dateUntil.nativeElement.value;
     console.log(this.asideHeaderService.dateSince)
