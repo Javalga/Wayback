3be129d0be2efb1e06e8301d4a75888f7be8b460
Javier Fernández Jiménez <javifav92@gmail.com>
2022-12-28 13:10:01 +0100

    a

--- angular-wayback/src/app/pages/returns/returns.component.ts
+++ angular-wayback/src/app/pages/returns/returns.component.ts
@@ -14,7 +14,7 @@ export class ReturnsComponent {
   public cols: string[];
   public rows: any
   public show = true;
-  public selected;
+  public selected: number[] = [];
   public incidences: Incidence[];
 
   constructor(private IncidenceService: IncidenceService, public asideHeaderService: AsideHeaderService) {
@@ -23,61 +23,62 @@ export class ReturnsComponent {
 
     let until = this.asideHeaderService.twoWeeksAgo();
     this.asideHeaderService.dateUntil = until;
-   
+
     this.IncidenceService.getIncidenceToReturn(
       this.asideHeaderService.dateSince,
       this.asideHeaderService.dateUntil
     ).subscribe((data: Incidence[]) => {
       this.incidences = data;
-    this.cols = [
-      'Índice',
-      'N* Expedición',
-      'Estado',
-      'Tipo de Incidencia',
-      'Nombre',
-      'Teléfono',
-      'Email',
-      'Dirección',
-      'CP',
-      'Población',
-      'F. Entrada',
-      'F. Salida',
-      'Próx. Entrega',
-      'Horario de Entrega',
-      'Almacén',
-    ];
-    this.rows = [];
+
+      this.cols = [
+        'Índice',
+        'N* Expedición',
+        'Estado',
+        'Tipo de Incidencia',
+        'Nombre',
+        'Teléfono',
+        'Email',
+        'Dirección',
+        'CP',
+        'Población',
+        'F. Entrada',
+        'F. Salida',
+        'Próx. Entrega',
+        'Horario de Entrega',
+        'Almacén',
+      ];
+      this.rows = [];
       for (let i = 0; i < this.incidences.length; i++) {
-          this.rows.push([
-            this.incidences[i].incidence_ref,
-            this.incidences[i].status,
-            this.incidences[i].incidence_type,
-            this.incidences[i].customer_name,
-            this.incidences[i].customer_phone,
-            this.incidences[i].customer_mail,
-            this.incidences[i].customer_address,
-            this.incidences[i].customer_cp,
-            this.incidences[i].customer_city,
-            this.incidences[i].input_date === null
-              ? null
-              : `${new Date(this.incidences[i].input_date).getDate()}-${
-                  new Date(this.incidences[i].input_date).getMonth() + 1
-                }-${new Date(this.incidences[i].input_date).getFullYear()}`,
+        this.rows.push([
+          this.incidences[i].incidence_ref,
+          this.incidences[i].status,
+          this.incidences[i].incidence_type,
+          this.incidences[i].customer_name,
+          this.incidences[i].customer_phone,
+          this.incidences[i].customer_mail,
+          this.incidences[i].customer_address,
+          this.incidences[i].customer_cp,
+          this.incidences[i].customer_city,
+          this.incidences[i].input_date === null
+            ? null
+            : `${new Date(this.incidences[i].input_date).getDate()}-${new Date(this.incidences[i].input_date).getMonth() + 1
+            }-${new Date(this.incidences[i].input_date).getFullYear()}`,
 
-            this.incidences[i].output_date === null
-              ? null
-              : `${new Date(this.incidences[i].output_date).getDate()}-${
-                  new Date(this.incidences[i].output_date).getMonth() + 1
-                }-${new Date(this.incidences[i].output_date).getFullYear()}`,
+          this.incidences[i].output_date === null
+            ? null
+            : `${new Date(this.incidences[i].output_date).getDate()}-${new Date(this.incidences[i].output_date).getMonth() + 1
+            }-${new Date(this.incidences[i].output_date).getFullYear()}`,
 
-            this.incidences[i].next_delivery === null
-              ? null
-              : `${new Date(this.incidences[i].next_delivery).getDate()}-${
-                  new Date(this.incidences[i].next_delivery).getMonth() + 1
-                }-${new Date(this.incidences[i].next_delivery).getFullYear()}`,
-            this.incidences[i].delivery_time,
-            this.incidences[i].warehouse,
-          ]);
+          this.incidences[i].next_delivery === null
+            ? null
+            : `${new Date(this.incidences[i].next_delivery).getDate()}-${new Date(this.incidences[i].next_delivery).getMonth() + 1
+            }-${new Date(this.incidences[i].next_delivery).getFullYear()}`,
+          this.incidences[i].delivery_time,
+          this.incidences[i].warehouse,
+        ]);
+
+        this.selected.push(i);
+        console.log(this.selected)
       }
     })
     console.log(this.asideHeaderService.dateSince);
