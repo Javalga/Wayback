90cf4597e79f71c8a520ef9f0a8c47575defc024
alanheredero11@gmail.com <alanheredero11@gmail.com>
2023-01-09 18:10:22 +0100

    customer solved

--- angular-wayback/src/app/pages/returns/returns.component.ts
+++ angular-wayback/src/app/pages/returns/returns.component.ts
@@ -7,21 +7,23 @@ import { PdfComponent } from 'src/app/components/pdf/pdf.component';
 @Component({
   selector: 'app-returns',
   templateUrl: './returns.component.html',
-  styleUrls: ['./returns.component.css']
+  styleUrls: ['./returns.component.css'],
 })
 export class ReturnsComponent {
-
-  public value: string = "Imprimir Listado de Devoluciones";
+  public value: string = 'Imprimir Listado de Devoluciones';
   public cols: string[];
   public rows = [];
   public show = true;
   public selected: number[] = [];
   public incidences: Incidence[];
   public returns_pdf: PdfComponent;
-  public filteredIncidences: Incidence[]
+  public filteredIncidences: Incidence[];
   public auxRows;
 
-  constructor(private IncidenceService: IncidenceService, public asideHeaderService: AsideHeaderService) {
+  constructor(
+    private IncidenceService: IncidenceService,
+    public asideHeaderService: AsideHeaderService
+  ) {
     this.returns_pdf = new PdfComponent();
 
     let since = this.asideHeaderService.startOfTime();
@@ -65,26 +67,28 @@ export class ReturnsComponent {
           this.incidences[i].customer_city,
           this.incidences[i].input_date === null
             ? null
-            : `${new Date(this.incidences[i].input_date).getDate()}-${new Date(this.incidences[i].input_date).getMonth() + 1
-            }-${new Date(this.incidences[i].input_date).getFullYear()}`,
+            : `${new Date(this.incidences[i].input_date).getDate()}-${
+                new Date(this.incidences[i].input_date).getMonth() + 1
+              }-${new Date(this.incidences[i].input_date).getFullYear()}`,
 
           this.incidences[i].output_date === null
             ? null
-            : `${new Date(this.incidences[i].output_date).getDate()}-${new Date(this.incidences[i].output_date).getMonth() + 1
-            }-${new Date(this.incidences[i].output_date).getFullYear()}`,
+            : `${new Date(this.incidences[i].output_date).getDate()}-${
+                new Date(this.incidences[i].output_date).getMonth() + 1
+              }-${new Date(this.incidences[i].output_date).getFullYear()}`,
 
           this.incidences[i].next_delivery === null
             ? null
-            : `${new Date(this.incidences[i].next_delivery).getDate()}-${new Date(this.incidences[i].next_delivery).getMonth() + 1
-            }-${new Date(this.incidences[i].next_delivery).getFullYear()}`,
+            : `${new Date(this.incidences[i].next_delivery).getDate()}-${
+                new Date(this.incidences[i].next_delivery).getMonth() + 1
+              }-${new Date(this.incidences[i].next_delivery).getFullYear()}`,
           this.incidences[i].delivery_time,
           this.incidences[i].warehouse,
         ]);
 
         this.selected.push(i);
-
       }
-    })
+    });
     this.auxRows = this.rows;
   }
 
@@ -92,37 +96,40 @@ export class ReturnsComponent {
     this.selected = selected;
   }
 
-  printSelected() {
-    let selectedRows = [];
-    this.selected.forEach((elem) => {
-      selectedRows.push(this.rows[elem]);
-      console.log(this.rows[elem])
-    })
-    console.log(selectedRows)
-    this.returns_pdf.returnsPagePdf(selectedRows)
-  }
+  // printSelected() {
+  //   let selectedRows = [];
+  //   this.selected.forEach((elem) => {
+  //     selectedRows.push(this.rows[elem]);
+  //     console.log(this.rows[elem])
+  //   })
+  //   console.log(selectedRows)
+  //   this.returns_pdf.returnsPagePdf(selectedRows)
+  // }
 
+  returnsPagePdf() {
+    this.returns_pdf.returnsPagePdf(this.incidences);
+  }
 
   useFilter(params: string[]) {
     function normalizeString(text: string) {
       text = text.toLowerCase();
-      text = text.replace(/á/gi, "a");
-      text = text.replace(/é/gi, "e");
-      text = text.replace(/í/gi, "i");
-      text = text.replace(/ó/gi, "o");
-      text = text.replace(/ú/gi, "u");
-      text = text.replace(/ñ/gi, "n");
+      text = text.replace(/á/gi, 'a');
+      text = text.replace(/é/gi, 'e');
+      text = text.replace(/í/gi, 'i');
+      text = text.replace(/ó/gi, 'o');
+      text = text.replace(/ú/gi, 'u');
+      text = text.replace(/ñ/gi, 'n');
       return text;
     }
-    let key = params[0]
-    if (params[1] !== "") {
+    let key = params[0];
+    if (params[1] !== '') {
       this.filteredIncidences = this.incidences.filter(function (elem) {
-        return normalizeString(elem[key]) === normalizeString(params[1])
-      })
-      this.incidences = this.filteredIncidences
-      this.rows = []
+        return normalizeString(elem[key]) === normalizeString(params[1]);
+      });
+      this.incidences = this.filteredIncidences;
+      this.rows = [];
       this.filteredIncidences.forEach((incidence, index, arr) => {
-        this.rows.push([])
+        this.rows.push([]);
         this.rows[index].push(
           incidence.incidence_ref,
           incidence.status,
@@ -135,22 +142,25 @@ export class ReturnsComponent {
           incidence.customer_city,
           incidence.input_date === null
             ? null
-            : `${new Date(incidence.input_date).getDate()}-${new Date(incidence.input_date).getMonth() + 1
-            }-${new Date(incidence.input_date).getFullYear()}`,
+            : `${new Date(incidence.input_date).getDate()}-${
+                new Date(incidence.input_date).getMonth() + 1
+              }-${new Date(incidence.input_date).getFullYear()}`,
 
           incidence.output_date === null
             ? null
-            : `${new Date(incidence.output_date).getDate()}-${new Date(incidence.output_date).getMonth() + 1
-            }-${new Date(incidence.output_date).getFullYear()}`,
+            : `${new Date(incidence.output_date).getDate()}-${
+                new Date(incidence.output_date).getMonth() + 1
+              }-${new Date(incidence.output_date).getFullYear()}`,
 
           incidence.next_delivery === null
             ? null
-            : `${new Date(incidence.next_delivery).getDate()}-${new Date(incidence.next_delivery).getMonth() + 1
-            }-${new Date(incidence.next_delivery).getFullYear()}`,
+            : `${new Date(incidence.next_delivery).getDate()}-${
+                new Date(incidence.next_delivery).getMonth() + 1
+              }-${new Date(incidence.next_delivery).getFullYear()}`,
           incidence.delivery_time,
-          incidence.warehouse,
-        )
-      })
+          incidence.warehouse
+        );
+      });
     } else {
       this.rows = this.auxRows;
     }
