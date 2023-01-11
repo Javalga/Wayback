bcd9bd6cae8235a0457acc6646a527de8eadcfca
alanheredero11@gmail.com <alanheredero11@gmail.com>
2023-01-11 10:52:47 +0100

    m

--- angular-wayback/src/app/pages/historical/historical.component.ts
+++ angular-wayback/src/app/pages/historical/historical.component.ts
@@ -43,8 +43,7 @@ export class HistoricalComponent {
         this.incidences = this.incidences.filter(
           (elem) => elem.warehouse_id == this.loginService.user.warehouse_id
         );
-      }
-      else if (this.loginService.user.role_id == 2) {
+      } else if (this.loginService.user.role_id == 2) {
         this.incidences = this.incidences.filter(
           (elem) => elem.location_id == this.loginService.user.location_id
         );
@@ -147,4 +146,69 @@ export class HistoricalComponent {
       this.rows = this.auxRows;
     }
   }
+
+  changeDate(){    
+  this.IncidenceService.getAllIncidence().subscribe((data: Incidence[]) => {
+    this.incidences = data;
+    this.cols = [
+      'N* Expedición',
+      'Estado',
+      'Tipo de Incidencia',
+      'Nombre',
+      'Teléfono',
+      'Email',
+      'Dirección',
+      'CP',
+      'Población',
+      'F. Entrada',
+      'F. Salida',
+      'Próx. Entrega',
+      'Horario de Entrega',
+      'Almacén',
+    ];
+
+    if (this.loginService.user.role_id == 3) {
+      this.incidences = this.incidences.filter(
+        (elem) => elem.warehouse_id == this.loginService.user.warehouse_id
+      );
+    } else if (this.loginService.user.role_id == 2) {
+      this.incidences = this.incidences.filter(
+        (elem) => elem.location_id == this.loginService.user.location_id
+      );
+    }
+    this.rows = [];
+    for (let i = 0; i < this.incidences.length; i++) {
+      this.rows.push([
+        this.incidences[i].incidence_ref,
+        this.incidences[i].status,
+        this.incidences[i].incidence_type,
+        this.incidences[i].customer_name,
+        this.incidences[i].customer_phone,
+        this.incidences[i].customer_mail,
+        this.incidences[i].customer_address,
+        this.incidences[i].customer_cp,
+        this.incidences[i].customer_city,
+        this.incidences[i].input_date === null
+          ? null
+          : `${new Date(this.incidences[i].input_date).getDate()}-${
+              new Date(this.incidences[i].input_date).getMonth() + 1
+            }-${new Date(this.incidences[i].input_date).getFullYear()}`,
+
+        this.incidences[i].output_date === null
+          ? null
+          : `${new Date(this.incidences[i].output_date).getDate()}-${
+              new Date(this.incidences[i].output_date).getMonth() + 1
+            }-${new Date(this.incidences[i].output_date).getFullYear()}`,
+
+        this.incidences[i].next_delivery === null
+          ? null
+          : `${new Date(this.incidences[i].next_delivery).getDate()}-${
+              new Date(this.incidences[i].next_delivery).getMonth() + 1
+            }-${new Date(this.incidences[i].next_delivery).getFullYear()}`,
+        this.incidences[i].delivery_time,
+        this.incidences[i].warehouse,
+      ]);
+    }
+  });
+  this.auxRows = this.rows;};
 }
